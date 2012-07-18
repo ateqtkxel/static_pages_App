1cc0a74b3e2a0bd9fc2633ea48e73d685fdcf975
ateqtkxel <ateq.ejaz@tkxel.com>
2012-07-18 16:58:41 -0400

    static pages application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StaticPagesApp::Application.config.secret_token = 'aaac94a160f3c8913a426ba71d8c30c73c49e687980db636dd6e36f162eec51dac92f895f49b399777b56c3bf2d30a75fa0ca051d621018fed850484eb799b5a'
