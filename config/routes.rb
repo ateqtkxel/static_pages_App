3aa78602ce61c1af84e4dd0bc365a94c810d7d56
ateqtkxel <ateq.ejaz@tkxel.com>
2012-07-19 11:23:49 -0400

    Changes finish

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ StaticPagesApp::Application.routes.draw do
   get "static_pages/home"
 
   get "static_pages/help"
+   get "static_pages/AboutUs"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,60 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+
+  # Test 1 :content test of home webpage
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_content('Sample App')
+    end
+   # Test 1.1:  This test check valid title of home page
+    it "should have the title 'home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+    end
+
+
+
+  end
+
+  # Test 2 :content test of home web page
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+    # Test 2.1:  This test check valid title of Help page
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+    end
+
+  end
+  # Test 3: check page existance /missing route etc
+
+  describe "About page" do
+
+
+
+
+    it "should have the content 'about us'" do
+      visit '/static_pages/AboutUs'
+      page.should have_content('About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/AboutUs'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
+    end
+
+
+
+  end
+
+end
