082e01472f75cc1cb8211f85496c67b88c020726
ateqtkxel <ateq.ejaz@yahoo.com>
2012-07-21 21:14:52 +0500

    Sign up successfully

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 StaticPagesApp::Application.routes.draw do
-
-  get "users/new"
-
   root to: 'static_pages#home'
-  match '/signup',  to: 'users#new'
+  resources :users
+   match '/signup',  to: 'users#new'
   match '/help',      to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,10 +4,22 @@ describe "User pages" do
 
   subject { page }
 
+
+
+
+
   describe "signup page" do
     before { visit signup_path }
 
     it { should have_selector('h1',    text: 'Sign up') }
     it { should have_selector('title', text: full_title('Sign up')) }
   end
+end
+
+describe "profile page" do
+  # Code to make a user variable
+  let(:user) { FactoryGirl.create(:user) }
+  before { visit user_path(user) }
+
+
 end
